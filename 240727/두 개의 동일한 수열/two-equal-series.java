05454d440cf13b6404ecdd45efaa7e7046b62197
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-07-27 21:16:09 +0900

    Title: 두 개의 동일한 수열, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240727/두 개의 동일한 수열/two-equal-series.java
+++ 240727/두 개의 동일한 수열/two-equal-series.java
@@ -32,6 +32,6 @@ public class Main {
                 break;
             }
          }
-         if (flag ==true) System.out.print("Yes");
+         if (flag == true) System.out.print("Yes");
     }
 }
