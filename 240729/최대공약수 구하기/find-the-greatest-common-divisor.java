fc85de688a873a5332b4e2cea3107968e0d8279e
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-07-29 11:56:42 +0900

    Title: 최대공약수 구하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240729/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        gcd(n, m);
+    }
+
+    public static void gcd(int n, int m) {
+        int gcd = 0;
+        for (int i = 1; i <= Math.min(n, m); i++) {
+            if (n % i == 0 && m % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.print(gcd);
+    }
+}
