74ea2f48f2132e12a9eb2c0989af60cabd91e6ba
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-04 20:56:13 +0900

    Title: 2021년 날짜의 유무, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240804/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        if (checkDay(M, D)) System.out.print("Yes");
+        else System.out.print("No");
+    }
+
+    public static boolean checkDay(int M, int D) {
+        if (1 <= M && M <= 12) {
+            if (M == 1 || M == 3 || M == 5 || M == 7 || M == 9 || M == 11){
+                if (1 <= D && D <= 31) return true;
+                else return false;
+            }
+            if (M == 2) {
+                if (1 <= D && D <= 28) return true;
+                else return false;
+            }
+            else {
+                if (1 <= D && D <= 30) return true;
+                else return false;
+            }
+        }
+        
+        return false;
+    }
+}
