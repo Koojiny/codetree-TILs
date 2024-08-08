6ec7f61d8a2d9f430f3e4c2766cde664a24d4243
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-08 21:38:51 +0900

    Title: 함수를 이용한 윤년 판별, Time: 164ms, Memory: 8MB, Status: Passed - CodeTree

--- 240808/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240808/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -11,8 +11,7 @@ public class Main {
     }
 
     public static boolean isLunaryear(int year) {
-        if (year % 4 == 0) return true;
-        if (year % 100 == 0 && year % 400 != 0) return false;
+        if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)) return true;
         return false;
     }
 }
