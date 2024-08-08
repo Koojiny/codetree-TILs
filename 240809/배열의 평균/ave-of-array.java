38679d3c35528a2d3a9498779cfc68a279cb1b12
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-09 00:13:36 +0900

    Title: 배열의 평균, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240809/배열의 평균/ave-of-array.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        double[][] arr = new double[2][4];
+
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = Double.parseDouble(st.nextToken());
+            }
+        }
+        double a1, a2, b1, b2, b3, b4, c1 = 0.0;
+        a1 = (arr[0][0] + arr[0][1] + arr[0][2] + arr[0][3]) / 4;
+        a2 = (arr[1][0] + arr[1][1] + arr[1][2] + arr[1][3]) / 4;
+        b1 = (arr[0][0] + arr[1][0]) / 2;
+        b2 = (arr[0][1] + arr[1][1]) / 2;
+        b3 = (arr[0][2] + arr[1][2]) / 2;
+        b4 = (arr[0][3] + arr[1][3]) / 2;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                c1 += arr[i][j];
+            }
+        }
+        c1 = c1 / 8;
+
+        System.out.println(a1 + " " + a2);
+        System.out.println(b1 + " " + b2 + " " + b3 + " " + b4);
+        System.out.println(c1);
+    }
+}
