3331a37a6664d47c4bab5964b08c6ef0abe9fe30
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-08 16:26:22 +0900

    Title: 특정 숫자 도달하기, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240808/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            
+            if (a < 250) {
+                arr[i] = a;
+            } else break;
+        }
+        
+        int sum = 0;
+        int cnt = 0;
+        double avg = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] != 0) {
+                sum += arr[i];    
+                cnt++;
+            }
+        }
+        avg = sum / (double)cnt; 
+
+        System.out.print(sum + " " + avg);
+    }
+}
