198fbf3e35b751c3d194fd9dfeede80cafb41c0f
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-27 21:43:37 +0900

    Title: Factorial, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240827/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+
+        return n * factorial(n - 1);
+    }
+}
