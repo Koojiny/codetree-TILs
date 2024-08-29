fd4dbcbc9e5e58dd41ba089fa7484e79b7780e8e
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-08-29 23:27:03 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240829/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
+++ 240829/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -1,38 +1,19 @@
 import java.io.*;
 
 public class Main {
-
-    public static int sum = 0;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n % 2 != 0) {
-            System.out.print(sumOddNum(n));
-        } else {
-            System.out.print(sumEvenNum(n));
-        }
-        
+        System.out.print(sumNum(n));
     }
 
-    public static int sumOddNum(int n) {
-        sum = 0;
+    public static int sumNum(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
         
-        for (int i = 1; i <= n; i += 2) {
-            sum += i;
-        }
-        
-        return sum;
-    }
-
-    public static int sumEvenNum(int n) {
-        sum = 0;
-
-        for (int i = 2; i <= n; i += 2) {
-            sum += i;            
-        }
-
-        return sum;
+        return sumNum(n - 2) + n;
     }
 }
