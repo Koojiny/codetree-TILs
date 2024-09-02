54044030862757000ff735553d47e5e3211c2283
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-02 16:25:55 +0900

    Title: 숫자의 조건 여부, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240902/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+
+        if (a >= 113) System.out.print(1);
+        else System.out.print(0);
+    }
+}
