ccba244a0e102b17e8c0ab8e227ee3fa0e841ed6
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-09 22:49:41 +0900

    Title: 그 계절, 그 날, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        System.out.print(checkSeason(checkDate(y, m, d)));
+    }
+
+    public static int checkDate(int year, int month, int day) {
+        int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        // 1. 윤년 체크
+        if (lunarCheck(year)) {
+            if (month == 2) {
+                if (day > 29) {
+                    return -1;
+                } else {
+                    return month;
+                }
+            }
+        }
+        else {
+            if (days[month - 1] < day) {
+                return -1;
+            } else return month;
+        }
+        return -1;
+    }
+
+    public static String checkSeason(int n) {
+        if (n == -1) return "-1";
+        if (n >= 3 && n <= 5) return "Spring";
+        if (n >= 6 && n <= 8) return "Summer";
+        if (n >= 9 && n <= 11) return "Fall";
+        else return "Winter";
+    }
+
+    public static boolean lunarCheck(int year) {
+        if (year % 4 == 0) return true;
+        else if (year % 4 == 0 && year % 100 == 0) return false;
+        else if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0) return true;
+        else return false;
+    }
+}
