4bc4992ccf54049f1df21b2ead577b06827ecb76
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-09 23:07:07 +0900

    Title: DateTime to DateTime, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int dayTime = (a - 11) * 24 * 60;
+        int hourTime = (b - 11) * 60;
+        int minTime = c;
+
+        int tmpMin = 11;
+
+        System.out.print(dayTime + hourTime + minTime - tmpMin);
+    }
+}
