73a11b607345abc493f33d3a8cd83bd415736544
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-10 23:29:50 +0900

    Title: 그 계절, 그 날, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 240910/그 계절, 그 날/that-season-that-day.java
+++ 240910/그 계절, 그 날/that-season-that-day.java
@@ -46,9 +46,7 @@ public class Main {
     }
 
     public static boolean lunarCheck(int year) {
-        if (year % 4 == 0) return true;
-        else if (year % 4 == 0 && year % 100 == 0) return false;
-        else if (year % 4 == 0 && (year % 100 == 0 && year % 400 == 0)) return true;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) return true;
         else return false;
     }
 }
