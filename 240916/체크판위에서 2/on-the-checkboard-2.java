8f2983b7c0768728e7f03e77e8144e12892a9708
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-16 14:27:09 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240916/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int R,C;
+    static String[][] graph;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        int answer = 0; 
+        graph = new String[R][C];
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < R; j++) {
+                graph[i][j] = st.nextToken();
+            }
+        }
+
+        for (int i = 0; i < R - 1; i++) {
+            for (int j = 0; j < C - 1; j++) {
+                if (graph[i][j].equals(graph[i + 1][j + 1])) continue;
+                answer++;
+            }
+        }
+        System.out.println(answer - 1);
+    }
+}
