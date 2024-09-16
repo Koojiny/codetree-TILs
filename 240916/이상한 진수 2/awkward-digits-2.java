ec4baf9b31c5a7e4b0b043c931e089f38c5174e4
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-16 15:39:39 +0900

    Title: 이상한 진수 2, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240916/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] num = br.readLine().toCharArray();
+        int max = Integer.MIN_VALUE;
+        
+        // 1. 2진법 수를 2번째 숫자부터 순환 (맨 앞은 0이 아님을 가정하므로)
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] == '1') {
+                num[i] = '0';
+
+                String s = "";
+                for (int j = 0; j < num.length; j++) {
+                    s += num[j];
+                }
+                int tmp = Integer.parseInt(s, 2); // 10진법 변환
+                max = Math.max(max, tmp);
+                num[i] = '1';
+            } else {
+                num[i] = '1';
+
+                String s = "";
+                for (int j = 0; j < num.length; j++) {
+                    s += num[j];
+                }
+                int tmp = Integer.parseInt(s, 2); // 10진법 변환
+                max = Math.max(max, tmp);
+                num[i] = '0';
+            }
+        }
+        // 2. 순환하며 해당 숫자를 0 또는 1로 변경
+        // 3. 10진법으로 숫자를 변환
+        System.out.print(max);
+    }
+}
