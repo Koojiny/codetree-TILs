f2b47521b4ca3d8c06dab87825ff1d073f3139c3
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-16 15:03:11 +0900

    Title: 체크판위에서 2, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

--- 240916/체크판위에서 2/on-the-checkboard-2.java
+++ 240916/체크판위에서 2/on-the-checkboard-2.java
@@ -1,32 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int R,C;
-    static String[][] graph;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        int answer = 0; 
-        graph = new String[R][C];
-        for (int i = 0; i < R; i++) {
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[r][c];
+        
+        int cnt = 0;    // 경우의 수
+
+        // 2차원 배열 입력 받기
+        for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < C; j++) {
-                graph[i][j] = st.nextToken();
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = st.nextToken().charAt(0);
             }
         }
 
-        for (int i = 1; i < R - 1; i++) {
-            int temp = 0;
-            for (int j = 1; j < C - 1; j++) {
-                if (temp > 2) continue;
-                if (graph[i][j].equals(graph[i + 1][j + 1])) continue;
-                answer++;
-                temp++;
+        char tmpC = arr[0][0]; // 시작 위치 값 Flag (이후 W, B 값 바꿔가며 진행)
+        // 2차원 배열 순회하며 완전탐색
+        // 최소 대각선 오른쪽 아래로 한 칸씩은 이동하고 진행해야 함
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                for (int k = i + 1; k < r - 1; k++) {
+                    for (int l = j + 1; l < c - 1; l++) {
+                        if (arr[0][0] != arr[i][j] && arr[i][j] != arr[k][l] && arr[k][l] != arr[r - 1][c - 1]) cnt++;
+                    }
+                }
             }
         }
-        System.out.println(answer);
+
+        System.out.print(cnt);
     }
 }
