45d68f5ac0a450e6bb8727a6c390b4e6d425cba9
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-21 22:31:49 +0900

    Title: 씨 오 더블유 2, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240921/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                for (int k = j + 1; k < s.length(); k++) {
+                    if (s.charAt(i) == 'C' && s.charAt(j) == 'O' && s.charAt(k) == 'W') cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
