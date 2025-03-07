85430588ad7aad0c664609953cd8cba42eebd32e
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-03-07 17:20:38 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250307/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int choo = 13;
+        double gravity = 0.165;
+
+        System.out.printf("%d * %f = %f", choo, gravity, choo * gravity);
+    }
+}
