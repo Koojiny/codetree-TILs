857833757b0dfc56374715785fea34f6f4215955
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-04-15 21:32:35 +0900

    Title: 함수를 이용한 소수 판별, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250415/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(sumPrime(a, b));
+    }
+
+    public static int sumPrime(int a, int b) {
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            if(isPrime(i)) sum += i;
+        }
+
+        return sum;
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }    
+}
