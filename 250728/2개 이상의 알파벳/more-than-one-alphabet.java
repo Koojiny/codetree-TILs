837c5e5e8b258d129755d1fd5d950683a08ad796
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-28 17:08:07 +0900

    Title: 2개 이상의 알파벳, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250728/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+
+        if (checkAlphabet(A)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean checkAlphabet(String s) {
+        boolean flag = false;
+        
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1)) flag = true;
+        }
+
+        return flag;
+    }
+}
