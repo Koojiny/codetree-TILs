8dd86c9a32ad1f55e5ab61d82563e11dc0682c0f
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-28 15:24:35 +0900

    Title: 2021년 날짜의 유무, Time: 96ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250728/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        if (isExist(M, D)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isExist(int m, int d) {
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int nDay = days[m];
+
+        if (nDay >= d) return true;
+        else return false;
+    }
+}
