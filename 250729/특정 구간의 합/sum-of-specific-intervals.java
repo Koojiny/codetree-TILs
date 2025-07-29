3203b1e925e6a7353a0e79d56abe9bd210e1ef13
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-29 11:30:29 +0900

    Title: 특정 구간의 합, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250729/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int N, M, result;
+    public static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a1 = Integer.parseInt(st.nextToken());
+            int a2 = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for (int j = a1 - 1; j < a2; j++) {
+                sum += arr[j];
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
