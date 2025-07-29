f829333af784dac9a6ebf68a973dab41df2c2a80
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-29 11:16:33 +0900

    Title: 나누고 빼면서 합하기, Time: 83ms, Memory: 9MB, Status: Passed - Codetree

--- 250729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
+++ 250729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        arr = new int[M];
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -22,14 +22,13 @@ public class Main {
 
     public static void calc() {
         while (true) {
+            result += arr[M - 1];
             if (M == 1) break;
 
             if (M % 2 == 1) {
                 M -= 1;
-                result += arr[M - 1];
             } else {
                 M /= 2;
-                result += arr[M - 1];
             }
         }
     }
