9b8d9f6012c368409c8a5b1a682e0e2f1e538304
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-31 13:28:49 +0900

    Title: 두 개의 동일한 수열, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250731/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] != b[i]) {
+                System.out.println("No");
+                return;
+            }
+        }
+        
+        System.out.println("Yes");
+    }
+}   
