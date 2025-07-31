dcedc73f2701708acbe62bbb85d3f915e4eb24d8
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-31 17:42:36 +0900

    Title: 중앙값 계산 2, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250731/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (i % 2 == 0) {
+                Arrays.sort(arr, 0, i);
+                System.out.print(arr[i / 2] + " ");
+            }
+        }
+    }
+}
