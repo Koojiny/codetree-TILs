a3c04f6000bf5c3fa7173bcc61f2d70dd835e22f
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-07-31 13:22:30 +0900

    Title: 단어 정렬, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250731/단어 정렬/sorting-words.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] sArr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            sArr[i] = br.readLine();
+        }
+
+        Arrays.sort(sArr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(sArr[i]);
+        }
+    }
+}
