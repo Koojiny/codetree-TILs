9f5c2b154263275e8b28bc73c7c9f048f579b045
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-01 20:35:44 +0900

    Title: 줄 세우기 2, Time: 114ms, Memory: 9MB, Status: Passed - Codetree

--- 250801/줄 세우기 2/line-up-students-2.java
+++ 250801/줄 세우기 2/line-up-students-2.java
@@ -1,5 +1,43 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        Student[] arr = new Student[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            arr[i] = new Student(height, weight, i + 1);
+        }
+
+        Arrays.sort(arr, new Comparator<Student>() {
+            @Override
+            public int compare(Student a, Student b) {
+                if (a.height != b.height) return a.height - b.height;
+                return b.weight - a.weight;
+            }
+        });
+
+        for (Student s : arr) {
+            System.out.println(s.height + " " + s.weight + " " + s.num);
+        }
+    }
+}
+
+class Student {
+    int height;
+    int weight;
+    int num;
+
+    public Student(int height, int weight, int num) {
+        this.height = height;
+        this.weight = weight;
+        this.num = num;
     }
 }
