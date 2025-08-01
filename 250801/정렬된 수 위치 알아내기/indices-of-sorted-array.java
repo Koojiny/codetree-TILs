86421db070b26b305c52848c063c7815d25c4b42
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-01 15:20:23 +0900

    Title: 정렬된 수 위치 알아내기, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250801/정렬된 수 위치 알아내기/indices-of-sorted-array.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        Num[] nums = new Num[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            
+            nums[i] = new Num(x, i);
+        }
+
+        Arrays.sort(nums, new Comparator<Num>() {
+            @Override
+            public int compare(Num a, Num b) {
+                if (a.num != b.num) return a.num - b.num;
+                return a.idx - b.idx;
+            }
+        });
+
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int originIdx = nums[i].idx;
+            answer[originIdx] = i + 1;
+        }
+
+        for (int a : answer) {
+            System.out.print(a + " ");
+        }
+    }
+}
+
+class Num {
+    int num;
+    int idx;
+
+    public Num(int num, int idx) {
+        this.num = num;
+        this.idx = idx;
+    }
+}
