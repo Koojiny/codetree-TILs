f66210f13eef51b46675f5c4a4468f8c4d09677f
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-03 23:11:30 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250803/Date to Date/date-to-date.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m1 = Integer.parseInt(st.nextToken());
+        int m1 = Integer.parseInt(st.nextToken());
+        int m1 = Integer.parseInt(st.nextToken());
+    }
+}
