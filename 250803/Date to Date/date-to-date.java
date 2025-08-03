734b7c48e175c16f1189fe5c80cbc635e60a05db
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-03 23:15:45 +0900

    Title: Date to Date, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 250803/Date to Date/date-to-date.java
+++ 250803/Date to Date/date-to-date.java
@@ -6,7 +6,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int m1 = Integer.parseInt(st.nextToken());
-        int m1 = Integer.parseInt(st.nextToken());
-        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(calcDay(m2, d2) - calcDay(m1, d1) + 1);
+    }
+
+    public static int calcDay(int m, int d) {
+        int[] day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days = 0;
+        for (int i = 1; i < m; i++) {
+            days += day[i];
+        }
+        days += d;
+
+        return days;
     }
 }
