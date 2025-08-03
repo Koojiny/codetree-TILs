2f6d54aee5d90e226844451c4faa2e3a63fc79b3
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-03 23:37:44 +0900

    Title: 요일 맞추기, Time: 80ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250803/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        String[] day = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        int idx = 1;
+        int diff = calcDay(m2, d2) - calcDay(m1, d1);
+        
+        System.out.println(day[(1 + diff) % 7]);
+    }
+
+    public static int calcDay(int m, int d) {
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        for (int i = 1; i < m; i++) {
+            totalDays += days[i];
+        }
+
+        totalDays += d;
+
+        return totalDays;
+    }
+}
