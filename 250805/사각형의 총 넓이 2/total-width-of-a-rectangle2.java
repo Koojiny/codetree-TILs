634ca000713dbbaf12507f95af44f62737ef3aac
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-05 11:56:21 +0900

    Title: 사각형의 총 넓이 2, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

--- 250805/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250805/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -6,9 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        int[][] board = new int[201][201];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());int x1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken()) + 100;
+            int y1 = Integer.parseInt(st.nextToken()) + 100;
+            int x2 = Integer.parseInt(st.nextToken()) + 100;
+            int y2 = Integer.parseInt(st.nextToken()) + 100;
+
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    board[j][k]++;
+                }
+            }
         }
+
+        int cnt = 0;
+        for (int i = 0; i < 201; i++) {
+            for (int j = 0; j < 201; j++) {
+                if (board[i][j] >= 1) cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
