583d2880b48c5c6c575aeb3b5c63676ab169ff10
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-08 23:47:55 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 106ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250808/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int OFFSET;
+    public static int[][] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        OFFSET = 10;
+        board = new int[OFFSET * 2 + 1][OFFSET * 2 + 1];
+
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int x2 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y2 = Integer.parseInt(st.nextToken()) + OFFSET;
+
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    board[j][k] = i + 1;
+                }
+            }
+        }
+
+        int maxGaro = 0;
+        int maxSero = 0;
+
+        // 가로 최대 길이
+        for (int j = 0; j < OFFSET * 2 + 1; j++) {
+            int cnt = 0;
+            for (int k = 0; k < OFFSET * 2 + 1; k++) {
+                if (board[j][k] == 1) cnt++;
+            }
+            if (cnt > maxGaro) maxGaro = cnt;
+        }
+
+        // 세로 최대 길이
+        for (int k = 0; k < OFFSET * 2 + 1; k++) {
+            int cnt = 0;
+            for (int j = 0; j < OFFSET * 2 + 1; j++) {
+                if (board[j][k] == 1) cnt++;
+            }
+            if (cnt > maxSero) maxSero = cnt;
+        }
+
+        System.out.println(maxGaro * maxSero);
+    }
+}
