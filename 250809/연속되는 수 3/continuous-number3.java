a3b5fad58d1a4a6d6ccf5bd06fc19102c0c868cc
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-09 20:08:35 +0900

    Title: 연속되는 수 3, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250809/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        boolean flag;
+        if (arr[0] > 0) flag = true;
+        else flag = false;
+
+        int cnt = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            boolean nflag = arr[i] > 0? true : false;
+            if (flag == nflag) {
+                cnt++;
+            } else {
+                cnt = 1;
+                flag = nflag;
+            }
+
+            if (cnt > max) max = cnt;
+        }
+
+        System.out.println(max);
+    }
+}
