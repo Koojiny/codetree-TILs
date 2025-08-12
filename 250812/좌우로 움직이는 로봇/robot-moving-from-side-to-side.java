a6645976c07a2b553df38ecd3b52cd8b8cff177e
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-12 13:49:05 +0900

    Title: 좌우로 움직이는 로봇, Time: 97ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250812/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250812/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int SIZE = 100;
+    public static final int SIZE = 1000000;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
