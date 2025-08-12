6bf39470db541e14534819b97ddf26fd90a3e96d
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-12 13:42:43 +0900

    Title: 좌우로 움직이는 로봇, Time: 96ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250812/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int SIZE = 1000000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] a = new int[SIZE];
+        int[] b = new int[SIZE];
+
+        int curTime = 1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            char d = st.nextToken().charAt(0);
+
+            while (t-- > 0) {
+                if (d == 'L') {
+                    a[curTime] = a[curTime - 1]--;
+                } else {
+                    a[curTime] = a[curTime - 1]++;
+                }
+                curTime++;
+            }
+        }
+
+        curTime = 1;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            char d = st.nextToken().charAt(0);
+
+            while (t-- > 0) {
+                if (d == 'L') {
+                    b[curTime] = b[curTime - 1]--;
+                } else {
+                    b[curTime] = b[curTime - 1]++;
+                }
+                curTime++;
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= curTime; i++) {
+            if (a[i - 1] != b[i - 1] && a[i] == b[i]) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
