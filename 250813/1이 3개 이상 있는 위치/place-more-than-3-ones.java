330cbd4930a7a335c0e0baf7b51eec967bee8968
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-13 15:35:04 +0900

    Title: 1이 3개 이상 있는 위치, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250813/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                        if (board[nx][ny] == 1) cnt++;
+                    }
+                }
+
+                if (cnt >= 3) ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
