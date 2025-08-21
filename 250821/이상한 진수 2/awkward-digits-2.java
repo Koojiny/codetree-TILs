904b25519709cd810f7b994ceeaf86c3ddba8a5a
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-21 11:09:55 +0900

    Title: 이상한 진수 2, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250821/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int len = s.length();
+        int[] arr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < len; i++) {
+            int num = 0;
+            arr[i] = 1 - arr[i];
+            for (int j = 0; j < len; j++) {
+                num += Math.pow(2, len - 1 - j) * arr[j];
+                // System.out.println("j= " + j + " "+ Math.pow(2, len - 1 - j) + " " + arr[j] + " " + num);
+            }
+            max = Math.max(max, num);
+            
+            arr[i] = 1 - arr[i];
+        }
+
+        System.out.println(max);
+    }
+}
