304c106feef9fad009932689295a0be252e529f5
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-24 14:21:58 +0900

    Title: 아름다운 수열 2, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 250824/아름다운 수열 2/beautiful-sequence-2.java
+++ 250824/아름다운 수열 2/beautiful-sequence-2.java
@@ -24,15 +24,15 @@ public class Main {
         int answer = 0;
         for (int i = 0; i <= n - m; i++) { // 수열 A를 모두 돌면서 M개를 모두 비교
             int cnt = 0;
+            boolean[] visit = new boolean[m];
             for (int j = i; j < i + m; j++) { // 현재 위치 i에서 M개를 비교
                 int cur = a[j];
-                boolean[] visit = new boolean[m];
-
+                
                 for (int k = 0; k < m; k++) {
                     if (cur == b[k] && !visit[k]) {
                         visit[k] = true;
                         cnt++;
-                        // System.out.println("j = " + j + " k = " + k + " ");
+                        // System.out.println("i = " + i + ", j = " + j + ", k = " + k + " ");
                         // System.out.println(Arrays.toString(visit));
                         break;
                     }
@@ -40,7 +40,10 @@ public class Main {
             }
             // System.out.println();
 
-            if (cnt == m) answer++;
+            if (cnt == m) {
+                answer++;
+                // System.out.println("정답 카운트 i = " + i);
+            }
         }
 
         System.out.println(answer);
