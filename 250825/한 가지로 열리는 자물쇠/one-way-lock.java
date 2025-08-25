5eedd320b2689faa32089471deb8db5ba519738c
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-25 22:01:05 +0900

    Title: 한 가지로 열리는 자물쇠, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250825/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <= N; k++) {
+                    int diffA = Math.abs(a - i);
+                    int diffA = Math.abs(a - i);
+                }
+            }
+        }
+    }
+}
